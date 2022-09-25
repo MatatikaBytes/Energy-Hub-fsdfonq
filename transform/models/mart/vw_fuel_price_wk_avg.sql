99fa74b9ecffb3ce55d69f871ff7db322abe03e6
Aaron Phethean <aphethean@matatika.com>
2022-09-25 14:27:53 +0100

    Add stacked datasets to show price components for fuel, duty, and VAT

--- transform/models/mart/vw_fuel_price_wk_avg.sql
+++ transform/models/mart/vw_fuel_price_wk_avg.sql
@@ -4,12 +4,26 @@ with weekly_fuel_prices as (
 
 ),
 
--- union all weekly energy price inputs
+-- calculate and union all weekly energy price inputs
+fuel_price_calc_vat as (
+
+  select
+    round(ulsd / ( 1 + ulsd_vat_pc), 2) as ulsd_ex_vat, 
+    round(ulsp / ( 1 + ulsp_vat_pc), 2) as ulsp_ex_vat, 
+    *
+  from weekly_fuel_prices
+
+),
+
 fuel_price_wk_avg as (
 
   select
+    (ulsd - ulsd_ex_vat) as ulsd_vat, 
+    (ulsp - ulsp_ex_vat) as ulsp_vat, 
+    (ulsd_ex_vat - ulsd_duty) as ulsd_ex, 
+    (ulsp_ex_vat - ulsp_duty) as ulsp_ex, 
     *
-  from weekly_fuel_prices
+  from fuel_price_calc_vat
 
 )
 
--- transform/models/staging/stg_weekly_fuel_prices.sql
+++ transform/models/staging/stg_weekly_fuel_prices.sql
@@ -14,12 +14,10 @@ cleaned as (
 		TO_DATE(source.date,'DD/MM/YYYY') as price_date,
 		CAST(source.ulsd AS DECIMAL) as ulsd, 
 		CAST(source.ulsd_duty AS DECIMAL) as ulsd_duty, 
-		round((CAST(source.ulsd AS DECIMAL) * (CAST(source.ulsd_vat AS DECIMAL) / 100)), 2) as ulsd_vat, 
-		CAST(source.ulsd_vat AS DECIMAL) as ulsd_vat_pc, 
+		(CAST(source.ulsd_vat AS DECIMAL) / 100) as ulsd_vat_pc, 
 		CAST(source.ulsp AS DECIMAL) as ulsp, 
 		CAST(source.ulsp_duty AS DECIMAL) as ulsp_duty, 
-		round((CAST(source.ulsp AS DECIMAL) * (CAST(source.ulsp_vat AS DECIMAL) / 100)), 2) as ulsp_vat, 
-		CAST(source.ulsp_vat AS DECIMAL) as ulsp_vat_pc 
+		(CAST(source.ulsp_vat AS DECIMAL) / 100) as ulsp_vat_pc 
 	from source
 	where TO_DATE(source.date,'DD/MM/YYYY') is not null -- drop any bad data
 ),
