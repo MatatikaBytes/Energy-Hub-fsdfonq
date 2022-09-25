0d3d119951bfb4d2a698d70ef897c7ffbc27193c
Aaron Phethean <aphethean@matatika.com>
2022-09-25 06:53:49 +0100

    Added a 3 year view of fuel price data

--- transform/models/staging/stg_weekly_fuel_prices.sql
+++ transform/models/staging/stg_weekly_fuel_prices.sql
@@ -14,10 +14,12 @@ cleaned as (
 		TO_DATE(source.date,'DD/MM/YYYY') as price_date,
 		CAST(source.ulsd AS DECIMAL) as ulsd, 
 		CAST(source.ulsd_duty AS DECIMAL) as ulsd_duty, 
-		CAST(source.ulsd_vat AS DECIMAL) as ulsd_vat, 
+		round((CAST(source.ulsd AS DECIMAL) * (CAST(source.ulsd_vat AS DECIMAL) / 100)), 2) as ulsd_vat, 
+		CAST(source.ulsd_vat AS DECIMAL) as ulsd_vat_pc, 
 		CAST(source.ulsp AS DECIMAL) as ulsp, 
 		CAST(source.ulsp_duty AS DECIMAL) as ulsp_duty, 
-		CAST(source.ulsp AS DECIMAL) as ulsp_vat 
+		round((CAST(source.ulsp AS DECIMAL) * (CAST(source.ulsp_vat AS DECIMAL) / 100)), 2) as ulsp_vat, 
+		CAST(source.ulsp_vat AS DECIMAL) as ulsp_vat_pc 
 	from source
 	where TO_DATE(source.date,'DD/MM/YYYY') is not null -- drop any bad data
 ),
